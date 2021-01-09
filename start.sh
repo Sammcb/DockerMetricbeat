3850fe6f0a4e43277f37acbdeaf9a76896587e88
Samuel McBroom <sam@sammcb.com>
2021-01-09 17:23:58 -0500

    Cleanup.

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
-service metricbeat start
-tail -f /dev/null
+while true; do
+	metricbeat setup -e -E output.logstash.enabled=false -E output.elasticsearch.enabled=true && break
+	sleep 5
+done
+metricbeat -e
